1680c05ba3b3ad7670624d726bd90d036a95a477
Olga Gabbert <olga.gabbert@gmail.com>
2023-03-12 21:17:53 +0100

    update props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,14 +6,36 @@ import SearchBar from '../SearchBar/SearchBar';
 
 import './App.css';
 
-function App() {
-  return(
-    <div className='App'>
-      <h1>ravenous</h1>
-      <SearchBar /> 
-      <BusinessList /> 
-    </div>
-  )
+let business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [business, 
+  business, 
+  business, 
+  business, 
+  business, 
+  business
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
